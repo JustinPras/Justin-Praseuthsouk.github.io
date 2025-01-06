0d7a1c9d0286f34fd0974797fcca9bc9ccd686a4
JustinPras <justinponpras@gmail.com>
2025-01-06 22:13:50 +1100

    updated main.js

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
-// Smooth scrolling on button click
-document.getElementById('scrollButton').addEventListener('click', function() {
-    document.getElementById('content').scrollIntoView({ behavior: 'smooth' });
-});
-
-// Trigger the fade-in effect when the content enters the viewport
-document.addEventListener('scroll', function() {
-    const content = document.getElementById('content');
-    const contentPosition = content.getBoundingClientRect().top;
-    const screenPosition = window.innerHeight / 1.3; 
-
-    if (contentPosition < screenPosition) {
-        content.classList.add('visible'); // Apply the fade-in effect
-    }
+document.getElementById('scrollButton').addEventListener('click', function () {
+    document.getElementById('content').scrollIntoView({
+        behavior: 'smooth'
+    });
 });
